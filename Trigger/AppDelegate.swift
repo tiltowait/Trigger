a7bb83ed4145d749c2cb5bc26daf3e0635cbf52e
tiltowait <tiltowaitt+github@icloud.com>
2021-02-06 10:38:31 -0800

    Initial commit

--- Trigger/AppDelegate.swift
+++ Trigger/AppDelegate.swift
@@ -7,33 +7,43 @@
 
 import Cocoa
 import SwiftUI
+import HotKey
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
   var window: NSWindow!
-
+  var statusBarItem: NSStatusItem!
+  let hotKey = HotKey(key: .grave, modifiers: [.control])
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Create the SwiftUI view that provides the window contents.
-    let contentView = ContentView()
-
-    // Create the window and set the content view.
-    window = NSWindow(
-        contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-        styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-        backing: .buffered, defer: false)
-    window.isReleasedWhenClosed = false
-    window.center()
-    window.setFrameAutosaveName("Main Window")
-    window.contentView = NSHostingView(rootView: contentView)
-    window.makeKeyAndOrderFront(nil)
+    self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+    
+    let menu = NSMenu()
+    let quitItem = NSMenuItem()
+    quitItem.title = "Quit Trigger"
+    quitItem.action = #selector(quit)
+    
+    menu.addItem(quitItem)
+    self.statusBarItem.menu = menu
+    
+    if let button = self.statusBarItem.button {
+      button.image = NSImage(named: "Icon")
+    }
+    
+    hotKey.keyDownHandler = {
+      let userDir = FileManager.default.homeDirectoryForCurrentUser
+      NSWorkspace.shared.open(userDir)
+    }
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
     // Insert code here to tear down your application
   }
 
+  @objc func quit(_ sender: AnyObject?) {
+    NSApp.terminate(self)
+  }
 
 }
 
